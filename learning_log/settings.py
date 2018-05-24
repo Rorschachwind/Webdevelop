512004b220c95409a3720681ffc47d165b416912
xiaopeng <xiaopeng0828@gmail.com>
2018-05-24 11:34:11 -0400

    some changes

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,7 +135,7 @@ if os.getcwd() == '/app':
 		'default':dj_database_url.config(default = 'postgres://localhost')
 	}
 
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWAEDED_PROTO','https')
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
 	ALLOWED_HOSTS = ['*']
 
